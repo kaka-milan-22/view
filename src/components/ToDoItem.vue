2fcea3efa87f31a05b3837404a6b407601707b06
Kaka <kaka@uponly.one>
2024-02-22 14:51:21 +0800

    first commit

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <input type="checkbox" id="todo-item" :checked="isDone" />
+      <label :for="id">{{ label }}</label>
+    </div>
+</template>
+
+<script setup>
+    import _ from 'lodash'
+
+    import {ref} from 'vue'
+    const props = defineProps({
+        label: {required: true, type: String},
+        done: {default: false, type: Boolean},
+        id: {required: true, type: String}
+    })
+
+</script>
---
+++ src/views/MeView.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul>
+    <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item 
+            :label="item.label" 
+            :done="item.done"
+            :id="item.id"></to-do-item>
+    </li>
+    </ul>
+</template>
+
+<script setup>
+import ToDoItem from '../components/ToDoItem.vue'
+import {ref} from 'vue'
+import _ from 'lodash'
+
+const ToDoItems = ref([
+    {id: _.uniqueId('todo-'), label: "Learn Vue", done: false},
+    {id: _.uniqueId('todo-'), label: "Create a Vue project with the CLI", done: true },
+    {id: _.uniqueId('todo-'), label: "Have fun", done: true },
+    {id: _.uniqueId('todo-'), label: "Create a to-do list", done: false },
+])
+</script>
